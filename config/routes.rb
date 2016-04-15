8941128f6301eac7add3f5741f6789ba9d1dac2b
kimaiga <nelsonkimaiga@aol.com>
2016-04-15 15:20:53 +0300

    Configured blog config routes, basic set-up complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :articles
+  root 'welcome#index'
+
   resources :articles do
     resources :comments
   end
